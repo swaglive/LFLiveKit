819157e32c7014fff03dc871653996ad31fa7e5e
finn <finn.he@swag.live>
2021-10-01 17:33:01 +0800

    update bridge header

--- LFLiveKit/capture/SW/SWVideoCapture.swift
+++ LFLiveKit/capture/SW/SWVideoCapture.swift
@@ -7,6 +7,42 @@
 
 import Foundation
 
-class SWVideoCapture: LFVideoCaptureInterface {
+@objcMembers
+public class SWVideoCapture: NSObject, LFVideoCaptureInterface {
+    public var running: Bool = true
+    public var delegate: LFVideoCaptureInterfaceDelegate?
+    public var preView: UIView!
+    public var captureDevicePosition: AVCaptureDevice.Position = .front
+    public var beautyFace: Bool = true
+    public var torch: Bool = false
+    public var mirror: Bool = true
+    public var zoomScale: CGFloat = 1.0
+    public var videoFrameRate: Int = 60
+    public var watermarkView: UIView? = nil
+    public var currentImage: UIImage? = nil
+    public var saveLocalVideo: Bool = false
+    public var saveLocalVideoPath: URL? = nil
+    public var currentColorFilterName: String? = nil
+    public var currentColorFilterIndex: Int = 0
+    public var colorFilterNames: [String]? = nil
+    public var mirrorOutput: Bool = true
+    private let configuration: LFLiveVideoConfiguration?
     
+    @objc
+    public required init?(videoConfiguration configuration: LFLiveVideoConfiguration?) {
+        self.configuration = configuration
+    }
+    
+    public func previousColorFilter() {
+        
+    }
+    
+    public func nextColorFilter() {
+        
+    }
+    
+    public func setTargetColorFilter(_ targetIndex: Int) {
+        
+    }
+
 }
